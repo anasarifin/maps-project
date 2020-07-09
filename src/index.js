4108b9678aff1b58f4ea305e5d338275b07e1250
ExoAcox <snsdkaskusone@gmail.com>
2020-07-09 16:27:01 +0700

    remove css import

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 // import * as serviceWorker from "./serviceWorker";
 import App from "./App";
-import "./index.css";
 import "regenerator-runtime";
 
 ReactDOM.render(<App />, document.getElementById("root"));
